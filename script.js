b72a14ff45f31492723cee485816b20bc0937f97
Hiruka <53434220+hiruka404@users.noreply.github.com>
2021-06-22 09:48:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "Adam";
+  sender = "KemalxIzanami";
 }
 
 let footer = document.getElementById("credit");
